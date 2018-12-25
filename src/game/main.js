a1016a3d270816156071caa616fef8538b49c559
Eemeli Kelokorpi <eemeli.kelokorpi@gmail.com>
2018-12-25 15:59:24 +0200

    Update

--- src/game/main.js
+++ src/game/main.js
@@ -8,8 +8,7 @@ game.addAsset('panda.png');
 game.createScene('Main', {
     init: function() {
         var sprite = new game.Sprite('panda.png');
-        sprite.x = 400;
-        sprite.y = 200;
+        sprite.center(this.stage);
         sprite.addTo(this.stage);
     }
 });
